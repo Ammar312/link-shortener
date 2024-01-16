6a5e90d36d114036c93b9b5c7afac19c1bafa7dc
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-01-16 09:28:55 +0500

    working ssr

--- index.mjs
+++ index.mjs
@@ -4,14 +4,21 @@ import "dotenv/config";
 import urlRoute from "./routes/url.mjs";
 import redirectRoute from "./routes/redirectUrl.mjs";
 import connectMongoDB from "./connectDB.mjs";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.use(express.json());
 
-connectMongoDB(process.env.MONGO_URI);
 // connectMongoDB(process.env.MONGO_URI).then(()=>console.log("connected Successfully"))
+connectMongoDB(process.env.MONGO_URI);
+
+app.use("/", (req, res) => {
+  return res.render("home");
+});
 app.use("/url", urlRoute);
 app.use("/link", redirectRoute);
 
